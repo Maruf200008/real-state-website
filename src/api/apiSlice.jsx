3c3a11933a761555d6ae43ce22cfffe65b1e5ffc
Mohammad Maruf <mohammadmarufgazi@gamil.com>
2023-07-16 21:23:59 +0600

    server connent sucessfully

--- src/api/apiSlice.jsx
+++ src/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://real-state-cj32.onrender.com",
   }),
   endpoints: (build) => ({}),
 });
